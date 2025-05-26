b84e1ce16776954e933554511631e50256f63695
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-25 21:12:43 -0300

    remove hardcoded drone thread instantiation in Main class

--- und1/src/main/java/Main.java
+++ und1/src/main/java/Main.java
@@ -4,14 +4,6 @@ import model.Posicao;
 public class Main {
 
     public static void main(String[] args) {
-        Thread droneNorte = new Thread(new Drone(Posicao.NORTE));
-        Thread droneSul = new Thread(new Drone(Posicao.SUL));
-        Thread droneLeste = new Thread(new Drone(Posicao.LESTE));
-        Thread droneOeste = new Thread(new Drone(Posicao.OESTE));
 
-        droneNorte.start();
-        droneSul.start();
-        droneLeste.start();
-        droneOeste.start();
     }
 }
