96cb99f6d2d0bb5cfc8ddcda49384c6cea7bab1e
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-25 21:11:50 -0300

    add server and drone executables; refactor Servidor to implement Runnable

---
+++ und1/src/main/java/executables/DroneExecutables.java
@@ -0,0 +1,18 @@
+package executables;
+
+import model.Drone;
+import model.Posicao;
+
+public class DroneExecutables {
+    public static void main(String[] args) {
+        Thread droneNorte = new Thread(new Drone(Posicao.NORTE));
+        Thread droneSul = new Thread(new Drone(Posicao.SUL));
+        Thread droneLeste = new Thread(new Drone(Posicao.LESTE));
+        Thread droneOeste = new Thread(new Drone(Posicao.OESTE));
+
+        droneNorte.start();
+        droneSul.start();
+        droneLeste.start();
+        droneOeste.start();
+    }
+}
