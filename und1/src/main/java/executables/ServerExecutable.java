24fb43c2bf2fe6d645a00eadbaa3ac1d5b7cdee3
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-25 21:12:52 -0300

    remove hardcoded drone thread instantiation in Main class

---
+++ und1/src/main/java/executables/ServerExecutable.java
@@ -0,0 +1,10 @@
+package executables;
+
+import model.Servidor;
+
+public class ServerExecutable {
+    public static void main(String[] args) {
+        Thread servidor = new Thread(new Servidor());
+        servidor.start();
+    }
+}
