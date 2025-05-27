bfd8f5b35b3d7d59de2772c566708c28e15b9796
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-27 12:38:48 -0300

    transforming the return object and padronizing drone data

--- und1/src/main/java/model/Servidor.java
+++ und1/src/main/java/model/Servidor.java
@@ -24,18 +24,50 @@ public class Servidor implements Runnable {
                 DatagramPacket pacote = new DatagramPacket(buffer, buffer.length);
                 socket.receive(pacote);
 
-                String mensagem = new String(pacote.getData(), 0, pacote.getLength());
-                System.out.println("Mensagem recebida do drone: " + mensagem);
+                String msgNaoTratada = new String(pacote.getData(), 0, pacote.getLength());
+                RetornoDrone retorno = transformarStringEmRetornoDrone(msgNaoTratada);
+                String msgTratada = padronizarMensagem(retorno);
+
+                System.out.println("Mensagem recebida do drone " + retorno.getPosicao() + ": " + msgTratada);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //Os dados devem ser armazenados no formato: [temperatura//umidade//pressao//radiacao].
-    public String padronizarMensagem(String msg) {
-        return "";
+    //lembrete: dados devem ser armazenados no formato [temperatura//umidade//pressao//radiacao].
+    public RetornoDrone transformarStringEmRetornoDrone(String mensagem) {
+        String[] partes = mensagem.split("\\*");
+        if (partes.length != 2) {
+            throw new IllegalArgumentException("Mensagem inválida: " + mensagem);
+        }
+        String dados = partes[0];
+        String posicaoStr = partes[1];
 
+        Posicao posicao;
+        try {
+            posicao = Posicao.valueOf(posicaoStr);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Posição inválida: " + posicaoStr);
+        }
+        return new RetornoDrone(dados, posicao);
+    }
+
+    public String padronizarMensagem(RetornoDrone msg) {
+        Posicao pos = msg.getPosicao();
+
+        switch (pos) {
+            case NORTE:
+                return msg.getMensagem().replace("-", "//");
+            case SUL:
+                return msg.getMensagem().replace("(", "").replace(")", "").replace(";", "//");
+            case LESTE:
+                return msg.getMensagem().replace("{", "").replace("}", "").replace(",", "//");
+            case OESTE:
+                return msg.getMensagem().replace("#", "//");
+            default:
+                throw new IllegalArgumentException("Posição do drone desconhecida: " + pos);
+        }
     }
 
 }
