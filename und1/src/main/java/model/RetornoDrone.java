a74ce07ceb9708a08ed6bed8a26c69a39eb02f7b
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-29 15:34:19 -0300

    refactor RetornoDrone and Servidor for object serialization and message handling

--- und1/src/main/java/model/RetornoDrone.java
+++ und1/src/main/java/model/RetornoDrone.java
@@ -1,6 +1,10 @@
 package model;
 
-public class RetornoDrone {
+import java.io.Serializable;
+
+public class RetornoDrone implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     String mensagem;
     Posicao posicao;
 
@@ -12,17 +16,16 @@ public class RetornoDrone {
     public String getMensagem() {
         return mensagem;
     }
+
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
     }
+
     public Posicao getPosicao() {
         return posicao;
     }
+
     public void setPosicao(Posicao posicao) {
         this.posicao = posicao;
-    }
-
-    public byte[] getBytes() {
-        return (mensagem + "*" + posicao.name()).getBytes();
     }
 }
