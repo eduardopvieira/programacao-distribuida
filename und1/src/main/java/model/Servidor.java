a60bbb8d5e357e9bfe57d247b92343b2c8602ce0
eduardopvieira <eduardoopazvieira@gmail.com>
2025-05-30 09:41:21 -0300

    criacao do servidor

--- und1/src/main/java/model/Servidor.java
+++ und1/src/main/java/model/Servidor.java
@@ -1,23 +1,19 @@
 package model;
 
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class Servidor implements Runnable {
 
-    public Servidor() {}
-
-    //commiting develop
     @Override
     public void run() {
-        int porta = 4446;
-        String grupo = "230.0.0.0";
+        int portaServidorDatacenter = 4447;
+        //ip do range de 224 ate 239
+        String grupoServidorDatacenter = "231.0.0.0";
 
-        try (MulticastSocket socket = new MulticastSocket(porta)) {
-            InetAddress grupoMulticast = InetAddress.getByName(grupo);
+        try (MulticastSocket socket = new MulticastSocket(portaServidorDatacenter)) {
+            InetAddress grupoMulticast = InetAddress.getByName(grupoServidorDatacenter);
             socket.joinGroup(grupoMulticast);
 
             byte[] buffer = new byte[1024];  // Ajuste se necessário
@@ -27,50 +23,14 @@ public class Servidor implements Runnable {
                 DatagramPacket pacote = new DatagramPacket(buffer, buffer.length);
                 socket.receive(pacote);
 
-                //erros ja sao lançados dentro dessa funçao, se ela der errado o programa inteiro morre
-                RetornoDrone retorno = desserializarRetornoDrone(pacote);
-
-                if (retorno != null) {
-                    String mensagemTratada = padronizarMensagem(retorno);
-                    System.out.println("Drone " + retorno.getPosicao() + ": " + mensagemTratada);
-                }
-
-
+                // Aqui você pode processar o pacote recebido
+                String mensagemRecebida = new String(pacote.getData(), 0, pacote.getLength());
+                System.out.println("Mensagem recebida: " + mensagemRecebida);
 
 
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    private RetornoDrone desserializarRetornoDrone(DatagramPacket pacote) {
-        try (
-                ByteArrayInputStream byteStream = new ByteArrayInputStream(pacote.getData(), 0, pacote.getLength());
-                ObjectInputStream in = new ObjectInputStream(byteStream)
-        ) {
-            return (RetornoDrone) in.readObject();
-        } catch (Exception e) {
-            System.err.println("Erro ao desserializar objeto: " + e.getMessage());
-            return null;
-        }
-    }
-
-
-    public String padronizarMensagem(RetornoDrone msg) {
-        Posicao pos = msg.getPosicao();
-
-        switch (pos) {
-            case NORTE:
-                return msg.getMensagem().replace("-", "//");
-            case SUL:
-                return msg.getMensagem().replace("(", "").replace(")", "").replace(";", "//");
-            case LESTE:
-                return msg.getMensagem().replace("{", "").replace("}", "").replace(",", "//");
-            case OESTE:
-                return msg.getMensagem().replace("#", "//");
-            default:
-                throw new IllegalArgumentException("Posição do drone desconhecida: " + pos);
         }
     }
 
