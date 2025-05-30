137c6109ab506f4a9940a3ee413a2008c5b86478
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-30 18:32:21 -0300

    instanciando dois servidores para fins de teste

--- und1/src/main/java/executables/ServerExecutable.java
+++ und1/src/main/java/executables/ServerExecutable.java
@@ -1,10 +1,13 @@
 package executables;
 
-import model.DataCenter;
+import model.Servidor;
 
 public class ServerExecutable {
     public static void main(String[] args) {
-        Thread servidor = new Thread(new DataCenter());
-        servidor.start();
+        Thread servidor1 = new Thread(new Servidor(true));
+        Thread servidor2 = new Thread(new Servidor());
+
+        servidor1.start();
+        servidor2.start();
     }
 }
