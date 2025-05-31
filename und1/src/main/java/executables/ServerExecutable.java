62a61da9dcbcc07bf38381eded19e31792343aaa
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-31 18:06:13 -0300

    refactoring constructor

--- und1/src/main/java/executables/ServerExecutable.java
+++ und1/src/main/java/executables/ServerExecutable.java
@@ -4,8 +4,8 @@ import model.Servidor;
 
 public class ServerExecutable {
     public static void main(String[] args) {
-        Thread servidor1 = new Thread(new Servidor(true));
-        Thread servidor2 = new Thread(new Servidor());
+        Thread servidor1 = new Thread(new Servidor(50001, true));
+        Thread servidor2 = new Thread(new Servidor(50002));
 
         servidor1.start();
         servidor2.start();
