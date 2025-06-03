eedb8b16422f35c9f261f4697c3578b261fb5f25
eduardopvieira <eduardoopazvieira@gmail.com>
2025-06-03 16:45:06 -0300

    (incompleto) inicio da implementacao do round robin

--- und1/src/main/java/model/LocServer.java
+++ und1/src/main/java/model/LocServer.java
@@ -16,10 +16,15 @@ public class LocServer implements Runnable {
     private MulticastSocket multicastLocServerServer;
     private ExecutorService threadPool = Executors.newFixedThreadPool(10);
     private ConsistentHash<Integer> consistentHash;
+    private List<Integer> roundRobin = Collections.synchronizedList(new ArrayList<>());
     private Set<Integer> availableServers = Collections.synchronizedSet(new HashSet<>());
 
-    public LocServer() {
-        this.consistentHash = new ConsistentHash<>(10, Collections.emptyList());
+    private boolean isRoundRobin;
+
+    public LocServer(boolean isRoundRobin) {
+
+        this.isRoundRobin = isRoundRobin;
+
     }
 
     @Override
@@ -62,7 +67,11 @@ public class LocServer implements Runnable {
 
     private synchronized void registerServer(int port) {
         if (availableServers.add(port)) {
-            consistentHash.add(port);
+            if (isRoundRobin) {
+                roundRobin.add(port);
+            } else {
+                consistentHash.add(port);
+            }
             System.out.println("Server registered: " + port);
             System.out.println("Available servers: " + availableServers);
         }
@@ -83,7 +92,13 @@ public class LocServer implements Runnable {
     private void handleClientConnection(Socket clientSocket) {
         try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
             String clientId = clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort();
-            Integer selectedPort = consistentHash.get(clientId);
+            Integer selectedPort;
+
+            if (isRoundRobin) {
+                selectedPort = roundRobin.get(0);
+            } else {
+                selectedPort = consistentHash.get(clientId);
+            }
 
             String response = (selectedPort != null) ?
                     "CONNECT_TO:" + selectedPort :
